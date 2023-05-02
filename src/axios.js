060753e8f4cbedd1dca36c579d682d600ad19ce9
cemilyurumez <cemil.yurumez@gmail.com>
2023-05-02 23:23:07 +0300

    base url updated

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import router from './router';
 
 const axiosClient = axios.create({
-  // base url change
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}api`
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 });
 
 axiosClient.interceptors.request.use((config)=>{
